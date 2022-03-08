0a0a1f5889cc10ae3324bd79c10bca1f3c8be592
Mr-Macko <max.allesch@gmx.de>
2022-03-09 00:27:37 +0100

    implemented firestore

--- components/Chat.js
+++ components/Chat.js
@@ -2,16 +2,61 @@ import React from 'react';
 import { View, Platform, KeyboardAvoidingView } from 'react-native';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat'
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_6hXP8NEwcextI1UPrNwSKXZiya1pabU",
+  authDomain: "chat-app-3d860.firebaseapp.com",
+  projectId: "chat-app-3d860",
+  storageBucket: "chat-app-3d860.appspot.com",
+  messagingSenderId: "943157882112",
+};
 
 export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
       messages: [],
+      uuid: 0,
+      user: {
+        _id: '',
+        name: '',
+        avatar: '',
+      },
     }
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
+
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+
   }
 
   componentDidMount() {
+    let { name } = this.props.route.params;
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          name: name,
+          avatar: 'https://placeimg.com/140/140/any',
+        }
+      });
+
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+
     this.setState({
       messages: [
         {
@@ -26,7 +71,7 @@ export default class Chat extends React.Component {
         },
         {
           _id: 2,
-          text: 'This is a system message',
+          text: `${name} has entered the chat`,
           createdAt: new Date(),
           system: true,
         },
@@ -34,10 +79,28 @@ export default class Chat extends React.Component {
     })
   }
 
+  // Add message to state
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessages();
+      }
+    );
+  }
+
+  //Add message to database
+  addMessages() {
+    const message = this.state.messages[0];
+    // add a new messages to the collection
+    this.referenceChatMessages.add({
+      _id: message._id,
+      text: message.text || '',
+      createdAt: message.createdAt,
+      user: this.state.user,
+    });
   }
 
   renderBubble(props) {
@@ -53,6 +116,34 @@ export default class Chat extends React.Component {
     )
   }
 
+  // On update, sets the messages' state with the current data
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar,
+        },
+      });
+    });
+    this.setState({
+      messages: messages,
+    });
+  };
+
+  componentWillUnmount() {
+    this.authUnsubscribe();
+    this.unsubscribe();
+  }
+
   render() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
@@ -66,7 +157,9 @@ export default class Chat extends React.Component {
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           user={{
-            _id: 1,
+            _id: this.state.user._id,
+            name: this.state.user.name,
+            avatar: this.state.user.avatar,
           }}
         />
         {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
